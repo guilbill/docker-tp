4f034e86f396744ece356f635b48d3566c178fc1
Guillaume BILLEY <guillaume.billey@pharmagest.com>
2019-06-06 16:33:53 +0200

    add redis

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 /**
  * Created by gbilley on 26/05/17.
  */
-
 const http = require('http');
-const port = 3000;
+const port = 3001;
 
-const server = http.createServer((req, res) => {
+const redis = require("async-redis");
+const redisClient = redis.createClient('durnal');
+var redisOk = true;
+redisClient.on("error", function (err) {
+  redisOk = false;
+})
+
+const server = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  var hello = 'Hello World \n';
+  var visits = 'No Redis';
+
+  if (redisOk) {
+    visits = await redisClient.incr("counter");
+  }
+  res.end(hello + visits);
 });
 
 server.listen(port, () => {
